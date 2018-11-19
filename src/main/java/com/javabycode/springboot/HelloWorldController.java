74328f87981872f4060fb06dc0179c98468174dc
E5000242 <E5000242@IGDCLT0156.internal.innovapptive.com>
2018-11-19 18:37:25 +0530

    hello

--- src/main/java/com/javabycode/springboot/HelloWorldController.java
+++ src/main/java/com/javabycode/springboot/HelloWorldController.java
@@ -8,8 +8,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloWorldController {
     @RequestMapping("/hello")
-    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
-        
+    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) throws InterruptedException {
+    	int iteratorValue = 20;
+		System.out.println("\n=================> OOM test started..\n");
+		for (int outerIterator = 1; outerIterator < 20; outerIterator++) {
+			System.out.println("Iteration " + outerIterator + " Free Mem: " + Runtime.getRuntime().freeMemory());
+			int loop1 = 2;
+			int[] memoryFillIntVar = new int[iteratorValue];
+			// feel memoryFillIntVar array in loop..
+			do {
+				memoryFillIntVar[loop1] = 0;
+				loop1--;
+			} while (loop1 > 0);
+			iteratorValue = iteratorValue * 5;
+			System.out.println("\nRequired Memory for next loop: " + iteratorValue);
+			Thread.sleep(1000);
+		}
         String message="You just create Spring Boot Example successfully";
         model.addAttribute("name", name);
         model.addAttribute("message", message);
